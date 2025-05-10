b4275c8537be6d35cd62cc6804c204229a6df16f
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-10 12:00:21 -0300

    criação do teste para comandos de aplicação.

---
+++ test_comandos_aplicacao.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+# get() - Navega para uma URL
+browser.get("https://www.saucedemo.com/")
+
+# title() - Retorna o título da página
+print("O t'titulo da página é: ", browser.title)
+
+# current_url() - Retorna a URL atual
+print("A URL atual é: ", browser.current_url)
+
+# page_source() - Retorna o código-fonte da página
+print("O código-fonte da página é: ", browser.page_source)

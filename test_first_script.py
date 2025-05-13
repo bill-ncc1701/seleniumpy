2481eae03eb325ff81a28fa5ef75fda79df97c84
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-13 07:03:20 -0300

    inclusão maximize window

--- test_first_script.py
+++ test_first_script.py
@@ -2,8 +2,9 @@ import time
 
 from selenium import webdriver
 
-browser = webdriver.Chrome()
 
+browser = webdriver.Chrome()
+browser.maximize_window()
 browser.get("https://google.com")
 time.sleep(5)
 browser.quit()

5ec9bdbf24d03af243d630580e43ad3549eaa2ea
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-14 20:38:10 -0300

    Inclusao do site para testes

--- test_comandos_dropdown.py
+++ test_comandos_dropdown.py
@@ -1,5 +1,6 @@
 import time
 
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.select import Select
@@ -12,7 +13,7 @@ browser.maximize_window()
 browser.get("http://192.168.56.103:8484/pratice-dropdowns.html")
 
 dropdown_products = Select (browser.find_element(By.XPATH,"//select[@id='first']"))
-dropdown_products.select_by_visible_text("Google")
+dropdown_products.select_by_visible_text("Microsoft")
 time.sleep(3)
 
 dropdown_animals = Select (browser.find_element(By.XPATH,"//select[@id='animals']"))
@@ -21,9 +22,9 @@ time.sleep(3)
 dropdown_animals.select_by_index(3) #Seleciona o item Avatar
 
 dropdown_food = Select (browser.find_element(By.XPATH,"//select[@id='second']"))
-dropdown_food.select_by_visible_text("Pizza")
+dropdown_food.select_by_visible_text("Pasta")
 time.sleep(3)
-dropdown_food.select_by_visible_text("Bonda")
+dropdown_food.select_by_visible_text("Sushi")
 time.sleep(3)
 
 
